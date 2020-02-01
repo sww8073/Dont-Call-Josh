f2f8c87a779816a75ec21fd8f310c75434e47433
jks7743 <jks7743@rit.edu>
2020-02-01 15:27:55 -0500

    page class changes

--- src/storagemanager/Page.java
+++ src/storagemanager/Page.java
@@ -9,16 +9,61 @@ import java.util.ArrayList;
 public class Page {
 
     private int pageId;
-    private ArrayList<Object[]> recordList; // Array list of all the record stored ona page
+    private int maxRecordsPerPage;
+    private int table;
+    private ArrayList<Object[]> recordList; // Array list of all the record stored on a page
+    private String[] dataTypes;
 
     /**
      * Page Constructor
      * @param pageId the page id
      * @param record1 the 1st record to be added to the page
      */
-    public Page(int pageId, Object[] record1) {
+    public Page(int pageId, int table, int maxRecordsPerPage, Object[] record1, String[] dataTypes) {
         this.pageId = pageId;
         this.recordList = new ArrayList<Object[]>();
+        this.table = table;
         this.recordList.add(record1); // add the first record to the beginning of the arrayList
+        this.maxRecordsPerPage = maxRecordsPerPage;
+        this.dataTypes = dataTypes;
     }
+
+    /**
+     * 
+     * @param record
+     * @param primaryKeys
+     * @param dataTypes
+     * @return
+     */
+    public boolean insertRecord(Object record, int[] primaryKeys, String[] dataTypes) {
+        
+        // Check if record is full
+        if (recordList.size() == maxRecordsPerPage) {
+            return false;
+        }
+
+        // TODO check if record exists
+
+        for (Object[] pageRecord : recordList) {
+            for (int key : primaryKeys) {
+
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * 
+     * @param table
+     * @return
+     */
+    public boolean inTable(int table) {
+        if (this.table == table) {
+            return true;
+        }
+        return false;
+    }
+
 }
