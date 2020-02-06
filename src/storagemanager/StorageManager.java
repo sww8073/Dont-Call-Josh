53f2bedbdac81bf0ab3a1cbaca94f3ff095cbb2d
jks7743 <jks7743@rit.edu>
2020-02-05 19:33:36 -0500

    add check for getRecord to see if page is in buffer

--- src/storagemanager/StorageManager.java
+++ src/storagemanager/StorageManager.java
@@ -93,15 +93,18 @@ public class StorageManager extends AStorageManager {
             throw new StorageManagerException("The table does not exist");
         }
 
-        // TODO buffer management table check
-
-        // for now I'll assume that the table is in the buffer.
-
         ArrayList<Integer> pages = tablePages.get(table);
         Object[] record = null;
         for (Integer pageNum: pages) {
+            // attempt to get the page
             Page page = buffer.get(pageNum);
-            record = page.getRecordFromPage(keyValue);
+            if (page != null) {
+                // if the page is not null, we can look for the record in the page
+                record = page.getRecordFromPage(keyValue);
+            } else {
+                // we need to read the page into the buffer from memory
+                // TODO read in page to memory using BufferManager
+            }
         }
         return record;
     }
