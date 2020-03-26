784ac5480827fd0c68b0aaa5eebd742e31a8bbde
jpt8558 <jpt8558@rit.edu>
2020-03-26 19:58:46 -0400

    update

--- src/dml/DMLTester.java
+++ src/dml/DMLTester.java
@@ -64,7 +64,7 @@ public class DMLTester {
 
 
         // You may need to modify some values to test on your system
-        String dbLoc = "C:\\Users\\Matthew\\Desktop\\DB\\data";
+        String dbLoc = "C:\\Users\\jtell\\Desktop\\Database\\Database2";
         int pageBufferSize = 20;
         int pageSize = 4096;
         AStorageManager sm;
@@ -293,11 +293,11 @@ public class DMLTester {
 //
 //        System.out.println("Restarting the database...");
 //
-        database = Database.getConnection(dbLoc, pageBufferSize, pageSize);
-
-        //testing delete
-        System.out.println("Testing update.... none should fail...");
-
+//        database = Database.getConnection(dbLoc, pageBufferSize, pageSize);
+//
+//        //testing delete
+//        System.out.println("Testing update.... none should fail...");
+//
         String update1 = "update foo set name = \"hello\" where id = 1;";
         String update2 = "update foo set amount = amount + 1.0 where id >= 2 and amount < 30.0;";
         String update3 = "update foo set name = \"hi\", amount = 4.0 where amount = 4.12 or id = 3;";
@@ -328,6 +328,7 @@ public class DMLTester {
                 System.err.println("Updating Table 1 failed...");
                 System.exit(1);
             }
+
             sm.terminateDatabase();
         } catch (StorageManagerException e){
             e.printStackTrace();
